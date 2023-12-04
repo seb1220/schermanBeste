5fe72d561fdf4d847e946ae9095cfcdb3f4268c9
DuSack1220 <sebastiankawicher@gmail.com>
2023-12-04 11:01:45 +0100

    added roboter project

---
+++ roboter/roboter/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace roboter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            robotField.LoadField("C:\\Users\\sebas\\source\\repos\\tmpScher\\roboter\\Aufgabe1.xml");
+
+            //robotField.DrawField();
+        }
+    }
+}
